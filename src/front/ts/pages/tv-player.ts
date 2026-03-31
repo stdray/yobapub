1a08f9d486a263a39c845c434a1d01d0b8cbac0f
stdray <stdray@3po.su>
2026-03-31 19:44:37 +0300

    refactor(tv-player): enhance logging for playback and rendering processes

--- src/front/ts/pages/tv-player.ts
+++ src/front/ts/pages/tv-player.ts
@@ -3,13 +3,16 @@ import { Page, RouteParams } from '../types/app';
 import { goBack } from '../router';
 import { TvKey } from '../utils/platform';
 import { pageKeys, clearPage } from '../utils/page';
+import { Logger } from '../utils/log';
 
 const $root = $('#page-tv-player');
 const keys = pageKeys();
+const plog = new Logger('tv-player');
 let hls: any = null;
 let video: HTMLVideoElement | null = null;
 
 function render(title: string): void {
+  plog.debug('render called', { title });
   $root.html(
     '<div class="tv-player">' +
       '<video class="tv-player__video" autoplay></video>' +
@@ -19,50 +22,85 @@ function render(title: string): void {
     '</div>'
   );
   video = $root.find('video')[0] as HTMLVideoElement;
+  plog.debug('video element rendered', { videoExists: !!video });
 }
 
 function startPlayback(streamUrl: string): void {
-  if (!video) return;
+  plog.debug('startPlayback called', { streamUrl, videoExists: !!video });
+  if (!video) {
+    plog.error('video element not found');
+    return;
+  }
   const HlsCtor = (window as any).Hls;
+  plog.debug('HLS check', { hlsExists: !!HlsCtor, hlsSupported: HlsCtor && HlsCtor.isSupported() });
   if (HlsCtor && HlsCtor.isSupported()) {
+    plog.info('Using HLS.js');
     hls = new HlsCtor({
       maxBufferLength: 30,
       maxMaxBufferLength: 60
     });
+    plog.debug('HLS instance created');
     hls.loadSource(streamUrl);
+    plog.debug('HLS source loaded', { streamUrl });
     hls.attachMedia(video);
+    plog.debug('HLS attached to video element');
     hls.on(HlsCtor.Events.ERROR, (_: unknown, data: { fatal: boolean; type: string }) => {
+      plog.error('HLS error', { fatal: data.fatal, type: data.type });
       if (data.fatal) {
         if (data.type === HlsCtor.ErrorTypes.MEDIA_ERROR) {
+          plog.warn('Media error, attempting recovery');
           hls.recoverMediaError();
         } else {
+          plog.error('Fatal HLS error, stopping playback');
           stopPlayback();
           goBack();
         }
       }
     });
   } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+    plog.info('Using native HLS playback');
     video.src = streamUrl;
+  } else {
+    plog.error('HLS not supported and native playback not available');
   }
 }
 
 function stopPlayback(): void {
-  if (hls) { hls.destroy(); hls = null; }
-  if (video) { video.pause(); video.src = ''; video = null; }
+  plog.debug('stopPlayback called');
+  if (hls) {
+    plog.debug('destroying HLS instance');
+    hls.destroy();
+    hls = null;
+  }
+  if (video) {
+    plog.debug('clearing video element');
+    video.pause();
+    video.src = '';
+    video = null;
+  }
 }
 
 const handleKey = (e: JQuery.Event): void => {
+  plog.debug('handleKey', { keyCode: e.keyCode });
   switch (e.keyCode) {
     case TvKey.Enter:
       if (video) {
-        if (video.paused) { video.play().catch(() => { /* ignore */ }); }
-        else { video.pause(); }
+        if (video.paused) {
+          plog.debug('play pressed');
+          video.play().catch((err) => {
+            plog.error('play() failed', { error: String(err) });
+          });
+        } else {
+          plog.debug('pause pressed');
+          video.pause();
+        }
       }
       e.preventDefault(); break;
     case TvKey.Return:
     case TvKey.Backspace:
     case TvKey.Escape:
     case TvKey.Stop:
+      plog.debug('stop/back pressed');
       stopPlayback();
       goBack();
       e.preventDefault(); break;
@@ -71,11 +109,16 @@ const handleKey = (e: JQuery.Event): void => {
 
 export const tvPlayerPage: Page = {
   mount(params: RouteParams): void {
+    plog.info('tvPlayerPage mount', {
+      channelTitle: params.channelTitle,
+      channelStream: params.channelStream ? params.channelStream.substring(0, 80) : 'N/A'
+    });
     render(params.channelTitle || '');
     startPlayback(params.channelStream || '');
     keys.bind(handleKey);
   },
   unmount(): void {
+    plog.debug('tvPlayerPage unmount');
     stopPlayback();
     keys.unbind();
     clearPage($root);
